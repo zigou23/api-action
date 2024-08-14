7d6b297ccbec0ec307601f85d83d6684d222dab4
zigou <40587032+zigou23@users.noreply.github.com>
2024-08-14 14:46:30 +0800

    Update bing_all_langs.py

--- python/bing_all_langs.py
+++ python/bing_all_langs.py
@@ -6,10 +6,12 @@ from datetime import datetime
 # 定义语言代码列表
 languages = ['hu-HU', 'en-US', 'en-CA', 'en-GB', 'en-IN', 'es-ES', 'fr-FR', 'fr-CA', 'it-IT', 'ja-JP', 'pt-BR', 'de-DE', 'zh-CN']  # 可以添加其他语言代码
 
-# 确保目标文件夹存在
-output_dir = './bing'
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+thisyear = datetime.now().year - (datetime.now().month == 1 and datetime.now().day == 1)
+directories = ['./bing', f'./bing/{thisyear}']
+# 确保所有目标文件夹存在
+for directory in directories:
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 # 遍历每种语言
 for lang in languages:
@@ -39,30 +41,43 @@ for lang in languages:
         images_info.append(image_info)
 
     # 定义与语言代码相关的文件路径
-    file_path = f'./bing/bing_{lang}.json'
+    file_path_current = f'./bing/bing_{lang}.json'
+    file_path_yearly = f'./bing/{thisyear}/bing_{lang}.json'
 
-    # 尝试读取现有的本地JSON文件
-    try:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            existing_images_info = json.load(file)
-    except FileNotFoundError:
-        # 如果文件不存在，则初始化为一个空列表
-        existing_images_info = []
+    # 函数读写数据
+    def read_json(file_path):
+        try:
+            with open(file_path, 'r', encoding='utf-8') as file:
+                return json.load(file)
+        except FileNotFoundError:
+            return []
+
+    def write_json(file_path, data):
+        with open(file_path, 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+
+    # 读取现有数据
+    existing_images_info_current = read_json(file_path_current)
+    existing_images_info_yearly = read_json(file_path_yearly)
 
     # 提取现有数据中的startdate列表
-    existing_dates = {image['date'] for image in existing_images_info}
+    existing_dates_current = {image['date'] for image in existing_images_info_current}
+    existing_dates_yearly = {image['date'] for image in existing_images_info_yearly}
 
     # 过滤新数据中没有的startdate并添加到现有数据中
     for image_info in images_info:
-        if image_info['date'] not in existing_dates:
-            existing_images_info.append(image_info)
+        if image_info['date'] not in existing_dates_current:
+            existing_images_info_current.append(image_info)
+        if image_info['date'] not in existing_dates_yearly:
+            existing_images_info_yearly.append(image_info)
 
     # 按日期倒序排序现有数据
-    existing_images_info.sort(key=lambda x: datetime.strptime(x['date'], '%Y%m%d'), reverse=True)
+    existing_images_info_current.sort(key=lambda x: datetime.strptime(x['date'], '%Y%m%d'), reverse=True)
+    existing_images_info_yearly.sort(key=lambda x: datetime.strptime(x['date'], '%Y%m%d'), reverse=True)
 
     # 将更新后的数据写回到本地JSON文件
-    with open(file_path, 'w', encoding='utf-8') as file:
-        json.dump(existing_images_info, file, ensure_ascii=False, indent=4)
+    write_json(file_path_current, existing_images_info_current)
+    write_json(file_path_yearly, existing_images_info_yearly)
 
     # 将数据写入以语言代码命名的每周JSON文件
     weekly_file_path = f'./bing/bing_weekly_{lang}.json'
